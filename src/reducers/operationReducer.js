84535d4ce14fd22919a666a0065301c9b9f624f8
Dakota Brown <koterthecoder@gmail.com>
2019-06-12 12:03:28 -0400

    fix octal eval issue

--- src/reducers/operationReducer.js
+++ src/reducers/operationReducer.js
@@ -1,17 +1,22 @@
 
-export default (state = {value: "0"}, action) => {
+export default (state = {value: "0", prevAction:""}, action) => {
     switch (action.type) {
         case 'INPUT_ACTION':
             return {
-                value: state.value === "0" ? action.payload : state.value + action.payload,
+                value: state.value === "0" ? action.payload : 
+                            (state.prevAction ==="+" || state.prevAction === "-" || state.prevAction === "*" || state.prevAction === "/") && action.payload === "0" ?
+                                state.value : state.value + action.payload,
+                prevAction: action.payload,
             }
         case 'EVALUATE_ACTION':
             return {
-                value: eval(state.value)
+                value: eval(state.value),
+                prevAction: action.payload,
             }
         case 'CLEAR_ACTION':
             return {
-                value: "0"
+                value: "0",
+                prevAction: action.payload,
             }
         default:
             return state
